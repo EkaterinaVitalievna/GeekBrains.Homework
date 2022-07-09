b747c5c66541c8ae5aa780232f612f76f9099f2f
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-09 18:17:25 +0300

    Add task2

---
+++ Seminar1.Projekt1.Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введете первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 > num1)
+{
+    Console.WriteLine("Наибольшее число: " + num2 + ", наименьшее число: " + num1);
+}
+else
+    if (num1 > num2)
+    {
+    Console.WriteLine("Наибольшее число: " + num1 + ", наименьшее число: " + num2);
+    }
+    else
+    {
+    Console.WriteLine(num1 + " = " + num2);
+    }
