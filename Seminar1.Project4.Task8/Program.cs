84907a72e4ec0d4da9d763a2c5d9a9127d176a33
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-10 18:37:46 +0300

    Add Task8

---
+++ Seminar1.Project4.Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введете положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int evennumber = 1;
+
+while (evennumber <= num)
+{
+    if (evennumber % 2 == 0)
+    {
+        Console.WriteLine(evennumber);
+        evennumber ++;   
+    }
+    else
+    {
+        evennumber ++;
+    }
+}
