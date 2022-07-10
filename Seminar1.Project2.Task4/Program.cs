d9ff3f61855876f711c4e5884c91db39902cd64c
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-10 14:30:37 +0300

    Add task 4

---
+++ Seminar1.Project2.Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введете первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > num1 && num2 > num3) max = num2;
+if (num3 > num1 && num3 > num2) max = num3;
+else num1 = max;
+
+Console.WriteLine("Наибольшее число " + max);
